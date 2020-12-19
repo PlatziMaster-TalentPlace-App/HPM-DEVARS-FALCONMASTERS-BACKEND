1d0e7c6c14110852dcdf68b26bfbfc6a28ad92d0
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2020-12-18 20:03:03 -0600

    feat: add applications routes

---
+++ routes/applications.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const ApplicationService = require('../services/applications');
+
+function applicationsApi(app) {
+  const router = express.Router();
+  app.use('/api/applications', router);
+
+  const applicationService = new ApplicationService();
+
+  router.get('/', async function (req, res, next) {
+    const { userId, status, page, limit } = req.query;
+
+    const applications = await applicationService.getApplicationsByUsers({
+      userId,
+      status,
+      page,
+      limit,
+    });
+
+    try {
+      res.status(200).json({
+        data: applications,
+        message: 'applications retrieved',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.get('/:applicationId', async function (req, res, next) {
+    const { applicationId } = req.params;
+    const application = await applicationService.getApplication(applicationId);
+
+    try {
+      res.status(200).json({
+        data: application,
+        message: 'application retrieved',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.post('/', async function (req, res, next) {
+    const application = req.body;
+    try {
+      const applicationId = await applicationService.createApplication(
+        application
+      );
+      res.status(201).json({
+        data: applicationId,
+        message: 'application created',
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+}
+
+module.exports = applicationsApi;
