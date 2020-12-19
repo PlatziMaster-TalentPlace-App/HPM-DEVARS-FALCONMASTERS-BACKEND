93b9ac2dc2f8570b3df79504d4ba82e4c501d6f2
Razielini <razielco@gmail.com>
2020-12-19 02:29:09 -0700

    Update Changes

--- utils/schemas/applications.js
+++ utils/schemas/applications.js
@@ -6,16 +6,32 @@ const { Schema } = mongoose;
 const applicationSchema = new Schema({
   vacantId: String,
   userId: String,
-  status: String,
+  status: {
+    type: [
+      {
+        type: String,
+        enum: [
+          'APPLY',
+          'ACEPTED_BY_COACH',
+          'CONTACT_BOSS',
+          'INTERVIEW_PROCESS',
+          'REFUSED',
+          'ACCEPTED',
+        ],
+      },
+    ],
+    default: ['APPLY'],
+  },
   urlCV: String,
+  userAnswer: String,
   coachRecommendation: String,
   createdAt: {
     type: Date,
-    default: Date.now(),
+    default: Date.now,
   },
   updatedAt: {
     type: Date,
-    default: Date.now(),
+    default: Date.now,
   },
 });
 
